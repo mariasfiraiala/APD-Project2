14cda3ac200b7870b7cee25f494c4a84fd200a61
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2023-12-02 21:02:12 +0200

    Add initial Dispatcher, Host methods
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -3,6 +3,7 @@
 import java.util.List;
 
 public class MyDispatcher extends Dispatcher {
+    private int lastWorker = -1;
 
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
@@ -10,5 +11,54 @@ public class MyDispatcher extends Dispatcher {
 
     @Override
     public void addTask(Task task) {
+        switch (algorithm) {
+            case ROUND_ROBIN -> RoundRobin(task);
+            case SHORTEST_QUEUE -> ShortestQueue(task);
+            case SIZE_INTERVAL_TASK_ASSIGNMENT -> SizeIntervalTaskAssignment(task);
+            case LEAST_WORK_LEFT -> LeastWorkLeft(task);
+        }
+    }
+
+    private void RoundRobin(Task task) {
+        hosts.get((++lastWorker) % hosts.size()).addTask(task);
+    }
+
+    private void ShortestQueue(Task task) {
+        int minQueue = Integer.MAX_VALUE;
+        Host minHost = new MyHost();
+
+        for (Host h : hosts) {
+            int tasks = h.getQueueSize();
+            tasks += ((MyHost)h).isExecuting() ? 1 : 0;
+
+            if (tasks < minQueue) {
+                minQueue = tasks;
+                minHost = h;
+            }
+        }
+
+        minHost.addTask(task);
+    }
+
+    private void SizeIntervalTaskAssignment(Task task) {
+        switch (task.getType()) {
+            case SHORT -> hosts.get(0).addTask(task);
+            case MEDIUM -> hosts.get(1).addTask(task);
+            case LONG -> hosts.get(2).addTask(task);
+        }
+    }
+
+    private void LeastWorkLeft(Task task) {
+        long minWork = Long.MAX_VALUE;
+        Host minHost = new MyHost();
+
+        for (Host h : hosts) {
+            if(h.getWorkLeft() < minWork) {
+                minWork = h.getWorkLeft();
+                minHost = h;
+            }
+        }
+
+        minHost.addTask(task);
     }
 }
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,22 +1,47 @@
 /* Implement this class. */
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MyHost extends Host {
+
+    private PriorityQueue<Task> pq = new PriorityQueue<Task>();
+    private Task executingTask;
+
+    private boolean isExecuting;
+
     @Override
     public void run() {
     }
 
+    public boolean isExecuting() {
+        return isExecuting;
+    }
+
+    public void setExecuting(boolean executing) {
+        isExecuting = executing;
+    }
+
     @Override
     public void addTask(Task task) {
+        pq.add(task);
     }
 
     @Override
     public int getQueueSize() {
-        return 0;
+        return pq.size();
     }
 
     @Override
     public long getWorkLeft() {
-        return 0;
+        long workLeft = 0;
+        for (Task t : pq) {
+            workLeft += t.getLeft();
+        }
+
+        workLeft += isExecuting() ? executingTask.getLeft() : 0;
+
+        return workLeft;
     }
 
     @Override
